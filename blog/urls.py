f87a0865217cea5335ffe66cad9f2810fb68449f
shubham <shubham190shubham@gmail.com>
2020-09-08 12:12:53 +0530

    added about, archive page and like/dislike buttons

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('archive/', views.archive_list, name='archive_list'),
+    path('about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -67,3 +67,10 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def archive_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/archive_list.html', {'posts': posts})
+
+def about(request):
+    return render(request, 'blog/about.html', )
