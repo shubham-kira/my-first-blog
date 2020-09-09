a52ad9d31e6c014bd7e988f5d1dae8de3857fe6d
shubham <shubham190shubham@gmail.com>
2020-09-09 09:42:34 +0530

    Mobile friendly and add host for local android testing

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zr+#tz##9t8abo8&gn4u&@yu#(%zw#rgadq&w_9mq1399oh_qc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '192.168.43.223']
 
 LOGIN_REDIRECT_URL='/'
 
